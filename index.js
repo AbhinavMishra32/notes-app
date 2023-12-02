7c15ef9039f79851f19b501a1c4427fe9ff58d28
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-12-02 16:07:58 +0530

    Update MongoDB connection URI and add dotenv dependency

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
+
+const PORT = 3000;
+
 mongoose.set('strictQuery', false);
+
+const uri = process.env.API_KEY;
 
 
 const start = async() => {
-    await mongoose.connect();
+    await mongoose.connect(uri);
 
     app.listen(PORT, ()=>{
-        console.log('App listening on port ' + PORT);
+        console.log(`App listening on port ${PORT}`);
     })
 }
 

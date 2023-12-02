72d7707b7c89f6462918faa5ddb5941e0c6d77bd
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-12-02 15:41:10 +0530

    Add express and mongoose dependencies, set up server and database connection

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-hello
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+mongoose.set('strictQuery', false);
+
+
+const start = async() => {
+    await mongoose.connect();
+
+    app.listen(PORT, ()=>{
+        console.log('App listening on port ' + PORT);
+    })
+}
+
+start();
